f4666c3fac666ecd70c0d948fa0ea044d39a656b
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-12-23 13:44:50 +0530

    Create all functions basic.cpp

---
+++ linked list/all functions basic.cpp
@@ -0,0 +1,88 @@
+    #include<iostream>
+using namespace std; 
+
+
+
+class Node{
+    public: 
+
+    int data; 
+    Node* next; 
+
+}; 
+void print(Node* node){
+    while(node!=NULL){
+        cout<<node->data<<"-"; 
+        node=node->next; 
+    }
+
+}
+// if the new node is to be inserted at the head of the linked list ; 
+
+Node* insert_head(Node*head,int new_data){
+    Node*new_node=new Node(); 
+    new_node->data=new_data; 
+    new_node->next=head; 
+    head=new_node; 
+     return head; 
+
+}
+void  insertAfter(Node*prev_node,int new_data){
+    // check if previous node is NULL; 
+    if(prev_node==NULL){
+        return prev_node; 
+        //i.e we return NULL; as previous node cannot be NULL; 
+        // it will be similar to inserting at the head of the linked list ; 
+
+    }
+    Node*new_node=new Node(); 
+    new_node->data=new_data; 
+    new_node->next=prev_node->next; 
+    prev_node->next=new_node; 
+    
+}
+void deleteNode(Node*head,int value){
+    Node*temp=head; 
+    Node*prev=NULL;
+    // if the node to be deleted is the head; 
+    if(temp!=NULL and temp->data==value){
+        head=temp->next; 
+        delete temp; 
+        return ; 
+    }
+    // if that is not the case then ; 
+    else{
+        while(temp!=NULL and temp->data!=value){
+            temp=temp->next; 
+            prev=temp; 
+        }
+        if (temp==NULL){
+            cout<<" the value does not exist in the linked list "; 
+
+            return ;  
+        }
+        
+        prev->next=temp->next; 
+        delete temp; 
+
+    }
+
+}
+int main(){
+    // create a linked list with four nodes . 
+    Node*head=new Node(); 
+    head->data=1; 
+    Node*second=new Node(); 
+    Node*third=new Node(); 
+    Node*fourth=new Node(); 
+    head->next=second; 
+    second->data=2; 
+    second->next=third; 
+    third->data=3; 
+    third->next=fourth; 
+    fourth->data=4; 
+    fourth->next=NULL; 
+   print(head); 
+   
+
+}
